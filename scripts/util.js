deddcf79864cc59d8cc84585501d96a058f71060
Ryan Ong <59072730+ryanozx@users.noreply.github.com>
2023-01-15 01:38:55 +0800

    Targeted audio sharing and reduces active video volume to 1%

--- scripts/util.js
+++ scripts/util.js
@@ -1,32 +1,41 @@
 navigator.mediaDevices.getDisplayMedia({audio: true}).then(stream => {
     if(!api_key) return alert('You must provide a Deepgram API Key in the options page.')
-    if(stream.getAudioTracks().length == 0) return alert('You must share your tab with audio. Refresh the page.')
-    const recorder = new MediaRecorder(stream, { mimeType: 'audio/webm' })
+    else if(stream.getAudioTracks().length == 0) {
+        alert('You must share your tab with audio. Refresh the page.');
+        location.reload();
+    } else {
+        const recorder = new MediaRecorder(stream, { mimeType: 'audio/webm' })
 
-    socket = new WebSocket('wss://api.deepgram.com/v1/listen?model=general-enhanced', ['token', api_key])
+        socket = new WebSocket('wss://api.deepgram.com/v1/listen?model=general-enhanced', ['token', api_key])
 
-    recorder.addEventListener('dataavailable', evt => {
-        if(evt.data.size > 0 && socket.readyState == 1) socket.send(evt.data)
-    })
+        recorder.addEventListener('dataavailable', evt => {
+            if(evt.data.size > 0 && socket.readyState == 1) socket.send(evt.data)
+        })
 
+        const videos = Array.from(document.getElementsByTagName("video"));
+        videos.map(video => video.addEventListener("play", (e) => {
+            video.volume = 0.01;
+        }))
 
-socket.onopen = () => { recorder.start(250) }
+        socket.onopen = () => { recorder.start(250) }
 
-socket.onmessage = msg => {
-    const { transcript } = JSON.parse(msg.data).channel.alternatives[0]
-    if(transcript) {
-        
-        updateSubs(transcript)
+        socket.onmessage = msg => {
+            const { transcript } = JSON.parse(msg.data).channel.alternatives[0]
+            if(transcript) {
+                
+                updateSubs(transcript)
+            }
+        }
     }
-}
 })
 
 function updateSubs(transcript) {
-  // var cursor = document.getElementById("hnr23Blur")
   console.log(transcript)
   var p = document.querySelector("#hnr23Blur p");
-  
-  p.innerHTML = transcript
+  if (p !== null)
+  {
+    p.innerHTML = transcript
+  }
 
 }
 
