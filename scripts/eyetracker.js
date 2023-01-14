e162477fae67493b123f02529eb85aefb8f2052f
Ryan Ong <59072730+ryanozx@users.noreply.github.com>
2023-01-14 20:46:03 +0800

    Circle follows eye movements

--- scripts/eyetracker.js
+++ scripts/eyetracker.js
@@ -2,14 +2,44 @@ document.body.onload = addDiv;
 
 function addDiv() {
     const newDiv = document.createElement("div");
+    
     newDiv.setAttribute("id", "hnr23Blur");
 
-    newDiv.style.cssText = 'position:absolute;width:50%;height:50%;opacity:0.05;z-index:100;background:#000;';
+    newDiv.style.cssText = 'position:fixed;width:50%;height:50%;opacity:0.9;z-index:100;background:#000;border-radius:50%';
 
-    document.addEventListener("mousemove", (e) => {
-        newDiv.style.left = (e.pageX - (parseInt(newDiv.style.width, 10) / 200) * window.innerWidth) + "px";
-        newDiv.style.top = (e.pageY - (parseInt(newDiv.style.height, 10) / 200) * window.innerHeight) + "px";
-    })
-    
     document.body.insertBefore(newDiv, document.body.firstChild);
+
+    const historyCount = 8;
+
+    var queue = [];
+    var sumX = 0;
+    var sumY = 0;
+    var idx = 0;
+
+    for (var i = 0; i < historyCount; ++i)
+    {
+        queue.push([newDiv.style.left, newDiv.style.top]);
+        sumX += queue[i][0];
+        sumY += queue[i][1];
+    }
+
+    webgazer.setGazeListener(function(data, elapsedTime) {
+        if (data == null) {
+            return;
+        }
+        sumX -= queue[idx][0];
+        sumY -= queue[idx][1];
+        queue[idx][0] = data.x;
+        queue[idx][1] = data.y;
+        sumX += data.x;
+        sumY += data.y;
+        newDiv.style.left = (sumX / historyCount - (parseInt(newDiv.style.width, 10) / 200) * window.innerWidth) + "px";
+        newDiv.style.top = (sumY / historyCount - (parseInt(newDiv.style.height, 10) / 200) * window.innerHeight) + "px";
+        ++idx;
+        if (idx == historyCount)
+        {
+            idx = 0;
+        }
+        setTimeout(50);
+    }).begin();
 }
